181799ce0262926b1ccae6bdfd38ce2a31b2e960
sayu349 <106569098+sayu349@users.noreply.github.com>
2022-11-16 15:27:32 +0900

    sample.xlsxのカラムが表示可能になりました。
    - 「データ読み込み→カラム抽出して表示」の部分が出来ました。
    - 「htmlにデータ貼り付け→データ読み込み」の部分はまだ未完成です。。

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
+from flask import Flask
+from flask import Flask, render_template
 
-UPLOAD_FOLDER = 'images'
-ALLOWED_EXTENSIONS = {'xlsx','csv'}
 
 app = Flask(__name__)
 
+#Flaskでカラムの候補を出力する
+import pandas as pd
+import numpy as np
+from flask import Flask,render_template
+
+def read_excel(filename="sample.xlsx"):
+    df = pd.read_excel(filename)
+    return df.columns
+
+data_list=read_excel()
+
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template("index.html", data_list=data_list)
