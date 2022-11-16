f6f02761178a27d5d0edb14316aa4e487c43f735
sayu349 <106569098+sayu349@users.noreply.github.com>
2022-11-16 17:37:03 +0900

    index→pythonで読み込みまで完了
    - データを読み込む際に、エクセルデータが文字型（str型）で入力されている問題を抱えていますが、それ以外は情報として取れそうです。

--- app.py
+++ app.py
@@ -3,19 +3,34 @@ from flask import Flask
 from flask import Flask, render_template
 
 
+
 app = Flask(__name__)
 
 #Flaskでカラムの候補を出力する
 import pandas as pd
 import numpy as np
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 def read_excel(filename="sample.xlsx"):
     df = pd.read_excel(filename)
     return df.columns
 
-data_list=read_excel()
 
+data_list=read_excel()
 @app.route("/")
 def index():
     return render_template("index.html", data_list=data_list)
+
+
+@app.route("/sampleform-post",methods=["POST"])
+def sampleform():
+    print("受け取りました。")
+    amount = request.form["pm"]
+    risk = request.form["audit_risk"]
+    file = request.form["upload-file"]
+    print(request.form)
+    print(amount)
+    print(risk)
+    print(file)
+    print(type(file))
+    return f'受け取りました:{amount}'
